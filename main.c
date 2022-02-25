79bd7e1e5c39bb9f1c96b612bb60d1b6a7fb10fc
Батраков Кирилл Александрович <is141s01@awp10.csc.local>
2022-02-25 13:38:41 +0700

    edit main.c

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-
+#define board_size 9
 int main()
 {
-    printf("Hello, World");
+    char board[board_size][board_size]
+            = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+               {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+               {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+               {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+               {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+    for (int i = 0; i < board_size; i++) {
+        for (int j = 0; j < board_size; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
